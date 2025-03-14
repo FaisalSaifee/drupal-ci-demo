783ad9e9f7c3359811240eaf77d09acb31361a7d
faisalsaifeesystech <faisal.as@systechcorp.in>
2025-03-15 00:16:55 +0530

    Resolved the phpcs issues

--- web/modules/custom/ci_test/src/Controller/HelloController.php
+++ web/modules/custom/ci_test/src/Controller/HelloController.php
@@ -4,10 +4,16 @@ namespace Drupal\ci_test\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Provides a simple controller for testing CI.
+ */
 class HelloController extends ControllerBase {
 
   /**
    * Simple Hello World page.
+   *
+   * @return array
+   *   A renderable array with markup.
    */
   public function hello() {
     return [
